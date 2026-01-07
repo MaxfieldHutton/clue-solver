23c1d274ee2ced42d5441501ea55b0dd49d90c1c
Maxfield Hutton <maxfieldhutton@gmail.com>
2026-01-07 00:24:36 -0500

    support for up to 8 players on 1080p

--- main.py
+++ main.py
@@ -1,17 +1,81 @@
 import customtkinter as ctk # type: ignore
 
 
-
-class PlayerFrame(ctk.CTkFrame):
+class ClueFrame(ctk.CTkFrame):
 
     def __init__(self, master) -> None:
 
         super().__init__(master)
 
+        self.frame = ctk.CTkFrame(self)
+        self.frame.grid_rowconfigure(0, weight=0)
         
-    def setName(self, name: str) -> None:
-        self.label = ctk.CTkLabel(self, text=name, font=('Arial', 14))
-        self.label.pack(padx=10, pady=10)
+        self.frame.grid_columnconfigure(1, minsize=3)
+        self.frame.grid_columnconfigure(2, minsize=3)
+        self.frame.grid_columnconfigure(3, minsize=3)
+
+        self.frame.grid_columnconfigure(0, weight=1)
+        self.frame.grid_columnconfigure(1, weight=0)
+        self.frame.grid_columnconfigure(2, weight=0)
+        self.frame.grid_columnconfigure(3, weight=0)
+
+        self.frame.pack()
+
+
+
+
+
+def add_clue(master: ctk.CTkFrame, clue: str) -> ClueFrame:
+    cf: ClueFrame = ClueFrame(master)
+
+    cf.frame.name = ctk.CTkLabel(cf.frame, text=clue, font=('Arial', 12), width=100, anchor="w").grid(row=0, column=0, padx=0, pady=0, sticky="w")
+
+    cf.frame.check_1 = ctk.CTkCheckBox(cf.frame, text="", width=18).grid(row=0, column=1, padx=0, pady=0)
+
+    cf.frame.check_2 = ctk.CTkCheckBox(cf.frame, text="", width=18).grid(row=0, column=2, padx=0, pady=0)
+
+    cf.frame.check_2 = ctk.CTkCheckBox(cf.frame, text="", width=18).grid(row=0, column=3, padx=0, pady=0)
+    return cf
+
+
+
+
+class PlayerFrame(ctk.CTkFrame):
+
+
+    def __init__(self, master) -> None:
+
+        super().__init__(master)
+
+        self.label = ctk.CTkLabel(self, text=f"{mf.player_entry_frame.player_entry.get()}", font=('Arial', 18))
+        self.label.pack(padx=10, pady=5)
+
+        self.frame = ctk.CTkFrame(self)
+        self.frame.grid_columnconfigure(0, weight=1)
+        self.frame.grid_rowconfigure(0, weight=1)
+        self.frame.pack(padx=5, pady=5)
+
+
+        self.label_frame: ctk.CTkFrame = ctk.CTkFrame(self.frame)
+        self.label_frame.grid_columnconfigure(0, weight=1)
+        self.label_frame.grid_rowconfigure(0, weight=1)
+        self.label_frame.grid(row=0, column=0, padx=3, pady=3)
+
+        self.they_have_this_label: ctk.CTkLabel = ctk.CTkLabel(self.label_frame, text="Has this |", font=('Arial', 10)).grid(row=0, column=1, padx=0, pady=3)
+        self.they_showed_me_this: ctk.CTkLabel = ctk.CTkLabel(self.label_frame, text="Showed me |", font=('Arial', 10)).grid(row=0, column=2, padx=0, pady=3)
+        self.i_showed_them_this: ctk.CTkLabel = ctk.CTkLabel(self.label_frame, text="Showed them", font=('Arial', 10)).grid(row=0, column=3, padx=0, pady=3)
+
+
+        self.clue_names: list[str] = ["Colonel Mustard","Miss Scarlet","Mrs Peacock","Mrs White",
+                                 "Mr Green","Prof Plum","Candlestick","Knife","Pipe",
+                                 "Revolver","Rope","Wrench","Kitchen","Ballroom","Conservatory",
+                                 "Dining Room","Billiard Room","Library","Lounge","Hall","Study"]
+        self.clues: list[ClueFrame] = []
+        for _row, name in enumerate(self.clue_names, start=1):
+            cf: ClueFrame = add_clue(self.frame, name)
+            cf.grid(row=_row, column=0, padx=0, pady=0)
+            self.clues.append(cf)
+        
 
 class PlayerEntryFrame(ctk.CTkFrame):
 
@@ -24,33 +88,32 @@ class PlayerEntryFrame(ctk.CTkFrame):
         self.text_label_frame = ctk.CTkFrame(self)
         self.text_label_frame.grid_columnconfigure(0, weight=1)
         self.text_label_frame.grid_rowconfigure(0, weight=1)
-        self.text_label_frame.pack(padx=10, pady=10)
+        self.text_label_frame.pack(padx=10, pady=5)
 
         self.label = ctk.CTkLabel(self.text_label_frame, text="Enter Player Name", font=('Arial', 14))
-        self.label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+        self.label.grid(row=0, column=0, padx=10, pady=5, sticky="w")
 
         self.player_entry = ctk.CTkEntry(self.text_label_frame)
         self.player_entry.bind("<KeyPress>", self.run_add_player)
-        self.player_entry.grid(row=0, column=1, padx=10, pady=10, sticky="e")
+        self.player_entry.grid(row=0, column=1, padx=10, pady=5, sticky="e")
         ####
 
-        self.button = ctk.CTkButton(self, text="Add Player")
-        self.button.pack(padx=10, pady=10)
+        self.button = ctk.CTkButton(self, text="Add Player", command=add_player_to_clue_solver)
+        self.button.pack(padx=10, pady=5)
 
     def run_add_player(self, event) -> None:
         if event.keysym == "Return":
             add_player_to_clue_solver()
     
 def add_player_to_clue_solver() -> None:
-    if len(cs.player_entry_frame.player_entry.get()) > 0:
-        print(f"Added Player {cs.player_entry_frame.player_entry.get()}")
-        cs.frame = PlayerFrame(cs)
-        cs.frame.setName(f"{cs.player_entry_frame.player_entry.get()}")
-        cs.frame.pack(padx=10, pady=10,side="left", expand=True, fill="both")
-        cs.player_entry_frame.player_entry.delete(0, 'end')
+    if len(mf.player_entry_frame.player_entry.get()) > 0:
+        print(f"Added Player {mf.player_entry_frame.player_entry.get()}")
+        mf.frame = PlayerFrame(mf)
+        mf.frame.pack(padx=10, pady=5,side="left", expand=True, fill="both")
+        mf.player_entry_frame.player_entry.delete(0, 'end')
 
 
-class ClueSolver(ctk.CTk):
+class MainFrame(ctk.CTk):
 
     def __init__(self) -> None:
         
@@ -61,26 +124,12 @@ class ClueSolver(ctk.CTk):
         self.geometry("1000x700")
 
         self.label = ctk.CTkLabel(self, text="Clue Solver", font=('Arial', 18))
-        self.label.pack(padx=10, pady=10)
+        self.label.pack(padx=10, pady=5)
 
         self.player_entry_frame = PlayerEntryFrame(self)
-        self.player_entry_frame.pack(padx=10, pady=10)
-      
-
-        
-        
-
-    def show_message(self) -> None:
-        for players in self.player_amount_box.get():
-            print(f"test {players}")
-
-    def shortcut(self, event) -> None:
-        if event.state == 4 and event.keysym == "Return":
-            self.show_message()
+        self.player_entry_frame.pack(padx=10, pady=5)
 
 
 
-
-
-cs = ClueSolver()
-cs.mainloop()
+mf = MainFrame()
+mf.mainloop()
